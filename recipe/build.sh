afe04e4d1cd3739da5d3bc880bece8da4cdb91db
Mark Harfouche <mark.harfouche@gmail.com>
2025-05-11 15:59:44 -0400

    cleanup recipe, don't fix anything

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,23 +1,15 @@
+set -ex
 
-##
-## START THE BUILD
-##
+# https://github.com/constantinpape/affogato/pull/26
+rm -rf cmake
 
 mkdir -p build
 cd build
 
-# Set the correct python flags, depending on the distribution
-PY_VER=$(python -c "import sys; print('{}.{}'.format(*sys.version_info[:2]))")
-PY_ABIFLAGS=$(python -c "import sys; print('' if sys.version_info.major == 2 else sys.abiflags)")
-PY_ABI=${PY_VER}${PY_ABIFLAGS}
+cmake                                 \
+    ${CMAKE_ARGS}                     \
+    -DPython_EXECUTABLE=${PYTHON}     \
+    ..
 
-##
-## Configure
-##
-cmake ${CMAKE_ARGS} .. -DPython_EXECUTABLE=${PYTHON}
-
-##
-## Compile and install
-##
 make -j${CPU_COUNT}
 make install
