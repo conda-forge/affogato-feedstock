bd5d3340799a93597ca2cd12272152f77e2c3323
Mark Harfouche <mark.harfouche@gmail.com>
2025-05-11 16:02:23 -0400

    Fail early on windows as well

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,12 +3,14 @@ cd build
 
 set CONFIGURATION=Release
 
-cmake .. -G "%CMAKE_GENERATOR%" ^
-         -DCMAKE_BUILD_TYPE=%CONFIGURATION% ^
-         -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+cmake .. -G Ninja                                  ^
+         -DCMAKE_BUILD_TYPE=%CONFIGURATION%        ^
+         -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%"    ^
          -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
-         -DBUILD_PYTHON=ON ^
-         -DBOOST_ROOT="%LIBRARY_PREFIX%" ^
-         -DPYTHON_EXECUTABLE="%PYTHON%"
+         -DBUILD_PYTHON=ON                         ^
+         -DBOOST_ROOT="%LIBRARY_PREFIX%"           ^
+         -DPython_EXECUTABLE="%PYTHON%"
+if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
 
 cmake --build . --config %CONFIGURATION% --target install
+if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
